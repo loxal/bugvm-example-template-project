ad66e35503ee1652cb53c590fe90c9f03f49528f
Alex <alexander.orlov@loxal.net>
2015-01-22 00:14:53 +0100

    cleanup

--- src/main/java/org/robovm/samples/helloworld/HelloWorld.java
+++ src/main/java/org/robovm/samples/helloworld/HelloWorld.java
@@ -1,20 +1,17 @@
 /*
- * Copyright (C) 2014 Trillian Mobile AB
- * 
+ * Copyright 2015 Alexander Orlov <alexander.orlov@loxal.net>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- *   
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
- * 
- * Portions of this code is based on Apple Inc's HelloWorld sample (v1.8)
- * which is copyright (C) 2008-2010 Apple Inc.
+ * limitations under the License.
  */
 
 package org.robovm.samples.helloworld;
@@ -28,7 +25,7 @@ import org.robovm.apple.uikit.UIWindow;
 import org.robovm.samples.helloworld.viewcontrollers.MyViewController;
 
 public class HelloWorld extends UIApplicationDelegateAdapter {
-    private UIWindow window;
+	private UIWindow window;
 	private MyViewController rootViewController;
 
 	public static void main(String[] args) {
@@ -37,24 +34,24 @@ public class HelloWorld extends UIApplicationDelegateAdapter {
 		pool.close();
 	}
 
-    @Override
-    public boolean didFinishLaunching (UIApplication application, UIApplicationLaunchOptions launchOptions) {
-        // Set up the view controller.
-	    rootViewController = new MyViewController();
+	@Override
+	public boolean didFinishLaunching(UIApplication application, UIApplicationLaunchOptions launchOptions) {
+		// Set up the view controller.
+		rootViewController = new MyViewController();
 
-        // Create a new window at screen size.
-        window = new UIWindow(UIScreen.getMainScreen().getBounds());
-        // Set our viewcontroller as the root controller for the window.
-	    window.setRootViewController(rootViewController);
-	    // Make the window visible.
-        window.makeKeyAndVisible();
+		// Create a new window at screen size.
+		window = new UIWindow(UIScreen.getMainScreen().getBounds());
+		// Set our viewcontroller as the root controller for the window.
+		window.setRootViewController(rootViewController);
+		// Make the window visible.
+		window.makeKeyAndVisible();
 
         /*
-         * Retains the window object until the application is deallocated. Prevents Java GC from collecting the window object too
-         * early.
+	 * Retains the window object until the application is de-allocated.
+	 * Prevents Java GC from collecting the window object too early.
          */
-        addStrongRef(window);
+		addStrongRef(window);
 
-        return true;
-    }
+		return true;
+	}
 }
