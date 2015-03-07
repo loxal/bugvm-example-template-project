6f6b2dc2c995300f93aa072d22bc5c2486e590f1
Alex <alexander.orlov@loxal.net>
2015-03-07 17:36:49 +0100

    broken state, clicking several times leads to runtime error

--- src/main/java/net/loxal/example/ios/view/RootViewController.java
+++ src/main/java/net/loxal/example/ios/view/RootViewController.java
@@ -16,17 +16,26 @@
 
 package net.loxal.example.ios.view;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.robovm.apple.coregraphics.CGRect;
 import org.robovm.apple.uikit.UIButton;
 import org.robovm.apple.uikit.UIButtonType;
 import org.robovm.apple.uikit.UIColor;
 import org.robovm.apple.uikit.UIControlState;
+import org.robovm.apple.uikit.UILabel;
 import org.robovm.apple.uikit.UIView;
 import org.robovm.apple.uikit.UIViewController;
 
-public class RootViewController extends UIViewController {
-    private int clickCount;
+import java.io.IOException;
+import java.net.URI;
 
+public class RootViewController extends UIViewController {
+    final HttpClient httpClient = new DefaultHttpClient();
+    final URI uri = URI.create("http://rest-kit-test-v1.test.cf.hybris.com/dilbert-quote/manager");
+    private int clickCount;
     public RootViewController() {
         final UIView view = getView();
         view.setBackgroundColor(UIColor.white());
@@ -35,10 +44,45 @@ public class RootViewController extends UIViewController {
         button.setFrame(new CGRect(100, 100, 90, 30));
         button.setTitle("Touch :)", UIControlState.Normal);
 
-        button.addOnTouchUpInsideListener((control, event) ->
-                        button.setTitle("Touch #" + ++clickCount, UIControlState.Normal)
-        );
+        final UILabel label = new UILabel();
+        label.setFrame(new CGRect(10.0, 240.0, 300.0, 100.0));
+        label.setText("This never happened before?!");
+        label.setBackgroundColor(UIColor.blue());
+        view.addSubview(label);
+
+        button.addOnTouchUpInsideListener((control, event) -> {
+            button.setTitle("Touch #" + ++clickCount, UIControlState.Normal);
+            label.setText("Touch #" + ++clickCount);
+            fetchManagerQuote();
+        });
 
         view.addSubview(button);
+    }
+
+    public void fetchManagerQuote() {
+//        Logger.getGlobal().info("hello");
+        try {
+            final HttpResponse response = httpClient.execute(new HttpGet(uri));
+//        val statusLine = response.getStatusLine()
+//        Logger.getGlobal().info("ere")
+//            return "blahaaaaa!";
+        } catch (IOException e) {
+//            e.printStackTrace();
+        }
+//        if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+//            val out = ByteArrayOutputStream()
+//            Logger.getGlobal().info("ere1")
+//            response.getEntity().writeTo(out)
+//            val responseContent = out.toString()
+//            out.close()
+//            Logger.getGlobal().info("ere2")
+//            Logger.getGlobal().info(responseContent)
+//            Logger.getGlobal().info("ere22")
+//            return responseContent
+//        } else {
+//            Logger.getGlobal().info("bad")
+//            response.getEntity().getContent().close()
+//        return "Locally it works!";
+//        }
     }
 }
--- src/main/kotlin/net/loxal/example/kotlin/ios/view/RootViewController.kt
+++ src/main/kotlin/net/loxal/example/kotlin/ios/view/RootViewController.kt
@@ -27,45 +27,58 @@ import org.robovm.apple.uikit.UIViewController
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.client.methods.HttpGet
 import java.net.URI
-import org.apache.http.HttpStatus
-import java.io.ByteArrayOutputStream
-import java.util.logging.Logger
+import org.robovm.apple.uikit.UILabel
+import org.robovm.apple.uikit.UITextView
 
 class RootViewController : UIViewController() {
-    private var clickCount: Int = 0
-
     {
-        val view = getView()
-        view.setBackgroundColor(UIColor.white())
+        getView().setBackgroundColor(UIColor.white())
+        getView().addSubview(createHelloWorldButton())
+    }
+    val t = UITextView.UITextViewPtr()
+    val l = UILabel.UILabelPtr()
+    val httpClient = DefaultHttpClient()
 
-        fun helloRest() {
-            val httpClient = DefaultHttpClient()
-            val response = httpClient.execute(HttpGet(URI.create("http://rest-kit-test-v1.test.cf.hybris.com/dilbert-quote/manager")))
-            val statusLine = response.getStatusLine()
-            if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
-                val out = ByteArrayOutputStream()
-                response.getEntity().writeTo(out)
-                val responseContent = out.toString()
-                out.close()
-                Logger.getGlobal().info(responseContent)
-            } else {
-                response.getEntity().getContent().close()
-            }
-        }
-        helloRest()
-
-        val helloWorldButton = createHelloWorldButton()
-        view.addSubview(helloWorldButton)
+    fun fetchManagerQuote(): String {
+        //        Logger.getGlobal().info("hello")
+        val response = httpClient.execute(HttpGet(URI.create("http://rest-kit-test-v1.test.cf.hybris.com/dilbert-quote/manager")))
+        //        val statusLine = response.getStatusLine()
+        //        Logger.getGlobal().info("ere")
+        //        if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+        //            val out = ByteArrayOutputStream()
+        //            Logger.getGlobal().info("ere1")
+        //            response.getEntity().writeTo(out)
+        //            val responseContent = out.toString()
+        //            out.close()
+        //            Logger.getGlobal().info("ere2")
+        //            Logger.getGlobal().info(responseContent)
+        //            Logger.getGlobal().info("ere22")
+        //            return responseContent
+        return "blahaaaaa"
+        //        } else {
+        //            Logger.getGlobal().info("bad")
+        //            response.getEntity().getContent().close()
+        //            return "Locally it works!"
+        //        }
     }
 
-    private fun createHelloWorldButton(): UIButton {
+    fun createHelloWorldButton(): UIButton {
         val button = UIButton.create(UIButtonType.System)
+
         button.setFrame(CGRect(60.0, 100.0, 200.0, 30.0))
-        button.setTitle("“Hello World” in Kotlin!", UIControlState.Normal)
+        button.setTitle("Next Quote", UIControlState.Normal)
+
+        val label = UILabel()
+        label.setFrame(CGRect(10.0, 240.0, 300.0, 100.0))
+        label.setText("This never happend before.")
+        label.setTintColor(UIColor.blue())
+        getView().addSubview(label)
 
         button.addOnTouchUpInsideListener(object : UIControl.OnTouchUpInsideListener {
             override fun onTouchUpInside(control: UIControl, event: UIEvent) {
-                button.setTitle("Touch #" + ++clickCount, UIControlState.Normal)
+                val managerQuote = fetchManagerQuote()
+                //                button.setTitle("${managerQuote}", UIControlState.Normal)
+                label.setText("${managerQuote}")
             }
         })
 
